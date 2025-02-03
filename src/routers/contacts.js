7513878730215182858cf52aa292db0badc7ed68
gizemcoban <gizem.coban.r@gmail.com>
2025-02-03 16:46:49 +0300

    all routes were added

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,1 +1,13 @@
-//server.jsdeki kod buraya tasinacak
+import { Router } from "express";
+import { getContactsController, getContactController, createContactController, patchContactController,deleteContactController, notFoundController } from './controllers/contacts.js';
+import ctrlWrapper from './utils/ctrlWrapper.js';
+
+const router  = Router();
+router.get('/contacts', ctrlWrapper(getContactsController));
+router.get('/contacts/:contactId',ctrlWrapper(getContactController));
+router.post('/contacts',ctrlWrapper(createContactController));
+router.patch('/contacts/:contactId',ctrlWrapper(patchContactController));
+router.delete('/contacts/:contactId',ctrlWrapper(deleteContactController));
+router.get('*', ctrlWrapper(notFoundController));
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -5,14 +5,15 @@
 //cors ve pino logger'ının ayarlanması
 //Mevcut olmayan rotalar için 404 hata durumu ve uygun mesaj döndürülmesi.{message: 'Not found'}
 // Sunucunun, PORT ortam değişkeni aracılığıyla belirtilen veya belirtilmemişse 3000 numaralı portta başlatılması
-//Sunucu başarıyla başlatıldığında konsola “Server is running on port {PORT}” mesajının yazdırılması; burada {PORT} sizin port numaranızdır
 
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino';
-import {getAllContacts, getContactById} from './services/contacts.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
+
+
+
 const setupServer = ()=>{
     const server = express();
 
@@ -32,72 +33,6 @@ const setupServer = ()=>{
 
     const PORT= Number(process.env.PORT || 3000 );
 
-
-    //getAllContacts getContactById
-
-
-    server.get('/contacts', async(req,res)=>{
-    
-    try{
-        const contacts = await getAllContacts();
-
-        res.status(200).json({
-            status: 200,
-            message: "Successfully found contacts!",
-            data: contacts,
-                // İstek işlenmesi sonucunda elde edilen iletişimler dizisi
-        });
-    }catch(error){
-        res.status(500).json({
-            status: 500,
-            message: "server error !",
-            data: error.message,
-        });
-    }
-
-});
-
-
-server.get('/contacts/:contactId', async(req,res)=>{
-    
-    try{
-        const {contactId} = req.params;
-        const contact = await getContactById(contactId);
-    
-        res.status(200).json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId} !`,
-            data:contact
-        });
-    
-        if(!contact){
-            res.status(404).json({
-                status: 404,
-                message: 'Contact not found',
-                data: contact,
-                //Verilen kimlik numarasıyla iletişimin bulunup bulunmadığını kontrol edin. Eğer iletişim bulunamazsa, 404 durum kodu ile aşağıdaki nesneyi döndürün
-                    // İstek işlenmesi sonucunda elde edilen iletişim
-            });
-            return;
-        }
-    }catch(error){
-        res.status(500).json({
-            status:500,
-            data: error.message,
-            message:"server error",
-        })
-    }
-
-
-});
-
-server.get('*',(req,res)=>{
-    
-    res.status(404).json({
-        status: 404,
-        message: "Not available route",
-    });
-});
 
 
 server.use(errorHandler);
